7db56c997918dc86f791e47e7e88a5f67d88e0c6
Sarah Hickey <sarahehickey@MacBook-Pro-2.local>
2024-04-28 16:50:13 -0400

    added rsa skeleton

---
+++ rsa.py
@@ -0,0 +1,29 @@
+#
+# Written by Cynthia Wang, Nikol Kovacevic, and Sarah Hickey for CS340's Final Project 
+# Spring 2024
+# 
+
+#TODO: 
+# 1. Implement basic RSA using user input for primes (foundation implementation) + checking for primes
+# 2. Implement RSA with generation of primes. Check that the primes are 
+# 
+# 
+# 
+# 
+# 
+
+def 
+
+# this will be problematic—maybe use a library or something to try to generate a prime number 
+def generate_primes(seed):
+    pass
+def generate_keys(p, q):
+    pass 
+def phi(pq):
+    pass 
+def encrypt(plaintext, key):
+    pass 
+def retrieve_private_key(phi):
+    pass 
+def decrypt(plaintext, key):
+    pass
